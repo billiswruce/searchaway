21bc0ddc2ba8dc29c0bcbc855d9b75463fecb122
Jessica Tell <hello.jessicatell@gmail.com>
2024-03-06 16:35:35 +0100

    joi och försöka skapa favoritbilder

---
+++ server/picsHandler.js
@@ -0,0 +1,24 @@
+const fs = require("fs").promises;
+const filePath = "./data.json";
+
+async function readData() {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    const parsedData = JSON.parse(data);
+    console.log(parsedData); // Lägg till denna loggning
+    return parsedData;
+  } catch (error) {
+    console.error("Read error:", error);
+    return [];
+  }
+}
+
+async function writeData(data) {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf8");
+  } catch (error) {
+    console.error("Write error:", error);
+  }
+}
+
+module.exports = { readData, writeData };
